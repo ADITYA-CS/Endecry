a42850b49d28ec7390a35b470baf0d861dd13682
Aditya Kumar Singh <cr7.aditya.cs@gmail.com>
2021-02-07 14:27:57 +0530

    Completed DES Encrypt function

--- src/cipher/des.cpp
+++ src/cipher/des.cpp
@@ -1,4 +1,5 @@
 #include "des.h"
+#include <algorithm>
 #include <iostream>
 
 void DES::GenerateKey(std::array<std::bitset<48>, 16> &K,
@@ -45,11 +46,57 @@ void DES::GenerateKey(std::array<std::bitset<48>, 16> &K,
       if (CD[id / 28][27 - id % 28])
         K[round][47 - i].flip();
     }
-    std::cout << "\"" << K[round] << "\"," << std::endl;
+//    std::cout << "\"" << K[round] << "\"," << std::endl;
   }
 }
 
-void DES::Encrypt(std::istream &, std::ostream &, const std::string &s) {
+void DES::Encrypt(std::istream &, std::ostream &, const std::string &s, bool decip) {
   std::array<std::bitset<48>, 16> key;
   GenerateKey(key, s);
+  if (decip) {
+    std::reverse(key.begin(), key.end());
+  }
+
+  std::vector<uint32_t> plaintext(2), ciphertext(2);
+  plaintext[0] = 19088743;
+  plaintext[1] = 2309737967;
+  ProcessBlock(plaintext, key, ciphertext);
+  std::string h{"0123456789ABCDEF"};
+  for (int i = 28; i >= 0; i-=4){
+    uint8_t t = (ciphertext[0] >> i) & 0xf;
+    std::cout << h[t];
+  }
+  for (int i = 28; i >= 0; i-=4){
+    uint8_t t = (ciphertext[1] >> i) & 0xf;
+    std::cout << h[t];
+  }
+}
+
+void DES::ProcessBlock(const std::vector<uint32_t> & plaintext, const std::array<std::bitset<48>, 16> & key, std::vector<uint32_t> & result) {
+  std::vector<uint32_t> LR(2);
+  for (int i = 0; i < 32; i++) {
+    uint8_t id = IP[i] - 1;
+    LR[0] |= ((plaintext[id >> 5] >> (31 - (id & 0x1f))) & 1) << (31 - i);
+  }
+  for (int i = 0; i < 32; i++) {
+    uint8_t id = IP[32 + i] - 1;
+    LR[1] |= ((plaintext[id >> 5] >> (31 - (id & 0x1f))) & 1) << (31 - i);
+  }
+  uint32_t  tmp;
+  for (uint8_t round = 0; round < 16; round++) {
+    tmp = FeistelNetwork::f(LR[1], key[round]);
+    tmp ^= LR[0];
+    LR[0] = LR[1];
+    LR[1] = tmp;
+  }
+  std::swap(LR[0], LR[1]);
+  result[0] = result[1] = 0;
+  for (uint8_t i = 0; i < 32; i++) {
+    uint8_t id = PI[i] - 1;
+    result[0] |= ((LR[id >> 5] >> (31 - (id & 0x1f))) & 1) << (31 - i);
+  }
+  for (uint8_t i = 0; i < 32; i++) {
+    uint8_t id = PI[32 + i] - 1;
+    result[1] |= ((LR[id >> 5] >> (31 - (id & 0x1f))) & 1) << (31 - i);
+  }
 }
